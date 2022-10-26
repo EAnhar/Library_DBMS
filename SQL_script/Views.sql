17164fedd1e06242c09068f3b5b3e9b540b34161
shathaa2 <92474113+shathaa2@users.noreply.github.com>
2022-10-26 12:14:48 -0800

    Update Views.sql

--- SQL_script/Views.sql
+++ SQL_script/Views.sql
@@ -3,3 +3,10 @@ SELECT  b.B_Name Book_Name ,A.Author_Name ,r.Availabilty
 FROM book b,Author A,resources r
 where  r.resourceID = b.ISBN  AND  b.AuthorID  = A.AuthorID ;
 
+
+
+CREATE VIEW LibraryADs AS
+SELECT Member_ID,First_Name,Last_Name,Member_Type,Email 
+FROM database_projrct.members;
+
+INSERT INTO LibraryADs( First_Name,Last_Name,Member_Type, Email )  value('LOLO','Al-fize','Undergraduate_Student','LOLO@qu.edu.sa');
