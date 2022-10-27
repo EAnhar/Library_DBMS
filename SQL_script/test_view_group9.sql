08647b1db990dafe8adb0cfc54acc2dffadb3fa0
Anhar Al_Dosarry <eanhar22@gmail.com>
2022-10-27 22:48:36 +0300

    Create test_view_group9.sql

---
+++ SQL_script/test_view_group9.sql
@@ -0,0 +1,5 @@
+\---------------------LibraryADs INSERT--------------------------\
+
+INSERT INTO LibraryADs( First_Name,Last_Name,Member_Type, Email )  
+value  ('Lolo','Al-fize','Undergraduate_Student','LOLO@qu.edu.sa');
+
