7c20fe2ef7c0e34c2ce02d1bcf27b9f578a71301
Anhar Al_Dosarry <eanhar22@gmail.com>
2022-10-27 23:05:47 +0300

    Delete test_view_group9.sql

--- SQL_script/test_view_group9.sql
+++
@@ -1,5 +0,0 @@
-\---------------------LibraryADs INSERT--------------------------\
-
-INSERT INTO LibraryADs( First_Name,Last_Name,Member_Type, Email )  
-value  ('Lolo','Al-fize','Undergraduate_Student','LOLO@qu.edu.sa');
-
