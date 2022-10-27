e7a0e1250cd8c0eaebfd96d25714963dc431190b
shathaa2 <92474113+shathaa2@users.noreply.github.com>
2022-10-27 09:43:51 -0800

    Update Views.sql

--- SQL_script/Views.sql
+++ SQL_script/Views.sql
@@ -5,8 +5,16 @@ where  r.resourceID = b.ISBN  AND  b.AuthorID  = A.AuthorID ;
 
 
 
-CREATE VIEW LibraryADs AS
-SELECT Member_ID,First_Name,Last_Name,Member_Type,Email 
-FROM database_projrct.members;
 
-INSERT INTO LibraryADs( First_Name,Last_Name,Member_Type, Email )  value('LOLO','Al-fize','Undergraduate_Student','LOLO@qu.edu.sa');
+CREATE VIEW Available_resources AS
+SELECT resourceID ,ResourceType,Availabilty,Privileges_LEVEL
+FROM resources1 r
+where  Availabilty = 'Available' ;
+
+
+
+CREATE ROLE  ResourcesRole;
+GRANT select ON Available_resources TO ResourcesRole;
+GRANT ResourcesRole TO Monerah;
+
+INSERT INTO vbookstatus4( resourceID,ResourceType,Availabilty,Privileges_LEVEL )  value(38,'BOOK',74,'Availabilty',3);
